13046817496a3c713a78d7ddc65fcfb7ced632a9
AndreaFigueroaR <afigueroa@fi.uba.ar>
2024-10-26 19:34:59 -0300

    feat(safeMap): permito lectura concurrente pero no a operaciones de escritura. Tambièn agrego funcionalidad para obtener/leer todas las claves

--- common/safeMap.h
+++ common/safeMap.h
@@ -1,10 +1,13 @@
 #ifndef SAFE_MAP_H_
 #define SAFE_MAP_H_
 
+#include <algorithm>
 #include <climits>
 #include <map>
 #include <mutex>
+#include <shared_mutex>
 #include <stdexcept>
+#include <vector>
 
 /* This is an implementation of a map that can be acced concurrently, the operations of
  * reading/writing are mutually exclusive and atomic*/
@@ -13,8 +16,9 @@ class SafeMap {
 private:
     std::map<K, V> map;
     const unsigned int max_size;
-
-    std::mutex mtx;
+    // mutex sharable to allow concurrent read operations , but still maaking the write operations
+    // mutually exclusive of itself and the rest of operations.
+    mutable std::shared_mutex rw_mtx;
 
 public:
     explicit SafeMap(unsigned int max_size): max_size(max_size) {}
@@ -22,6 +26,7 @@ public:
     /* Add an element in a non-blocking way; does not overwrites the value if the key already
      * exists, in that cse also returns false.*/
     bool tryInsert(const K& key, const V& value) {
+        // Exclusive lock for write operation
         std::unique_lock<std::mutex> lck(mtx);
         if (map.size() == max_size || map.find(key) != map.end()) {
             // if the map was fulled or if the key already was in the mp returns false
@@ -33,8 +38,8 @@ public:
 
     /* Removes an element in a non-blocking way; returns true if it existed and was deleted.*/
     bool tryErase(const K& key) {
+        // Exclusive lock for write operation
         std::unique_lock<std::mutex> lck(mtx);
-
         auto it = map.find(key);
         if (it == map.end()) {
             return false;
@@ -47,8 +52,8 @@ public:
      * that indicates whether the value was obtained and left in the received reference (true) or if
      * the value was not obtained (false)*/
     bool get(const K& key, V& value) const {
-        std::unique_lock<std::mutex> lck(mtx);
-
+        // Shared lock for read operation
+        std::shared_lock<std::shared_mutex> lck(rw_mtx);
         auto it = map.find(key);
         if (it == map.end()) {
             return false;
@@ -58,13 +63,26 @@ public:
         return true;
     }
 
+    /* Method to obtain a vector with all the keys in the map */
+    /* OJO: Implemento metodo para obtener un container con las claves, esto porque creo que luego
+     * de que un game termine y la logica del game solo tenga el id de un client, el ganador, se
+     * necesitarà instanciar una nueva ronda/game con los id que se guardaron aquì*/
+    std::vector<K> getKeys() const {
+        // Shared lock for read operation
+        std::shared_lock<std::shared_mutex> lck(rw_mtx);
+        std::vector<K> keys;
+        keys.reserve(map.size());
+
+        std::transform(map.begin(), map.end(), std::back_inserter(keys),
+                       [](const auto& pair) { return pair.first; });
+
+        return keys;
+    }
+
 private:
     SafeMap(const SafeMap&) = delete;
     SafeMap& operator=(const SafeMap&) = delete;
 };
 
-// OJO: Tambien me gustaria que se pueda obtener la losta de claves para poder (luego de que un game
-// termine y soo contenga a un id de un client) instanciar una nueva ronda/game con los ids que se
-// guardaron aquì
 
 #endif
