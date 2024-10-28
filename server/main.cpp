8ee5573d4b34989c2558da9bff1f00cfc6e58020
AndreaFigueroaR <afigueroa@fi.uba.ar>
2024-10-28 15:10:26 -0300

    feat(main server): add a first draw of the server main. Latter it will be encapsulated in a class server

--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,45 @@
+#include <iostream>
 
+#include "acceptor.h"
 
-int main() { return 0; }
+#define QUIT "q"
+
+void startDummyMatch(const char* port);
+
+int main(int argc, char* argv[]) {
+    try {
+        if (argc >= 2) {
+
+            startDummyMatch(argv[1]);
+            return 0;
+
+        } else {
+            std::cerr << "Bad program call. Expected " << argv[0] << " <servername>" << std::endl;
+            return 1;
+        }
+    } catch (const std::exception& err) {
+        std::cerr << "Something went wrong and an exception was caught: " << err.what()
+                  << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Something went wrong and an unknown exception was caught." << std::endl;
+        return 1;
+    }
+}
+
+void holdClosure() {
+    std::string in;
+    do {
+        std::cin >> in;
+    } while (in != QUIT);
+}
+
+void startDummyMatch(const char* port) {
+    AcceptorThread* acceptor = new AcceptorThread(port);
+    acceptor->start();
+    holdClosure();
+    // kill the match and the clients connected
+    acceptor->forceClosure();
+    acceptor->join();
+    delete acceptor;
+}
