9a359a987db713f425c3869c376e51d1aca65339
candematelica <candela.matelica@gmail.com>
2024-10-29 16:18:47 -0300

    Updated names and added stopMoving to protocolCodes

--- common/protocolCodes.h
+++ common/protocolCodes.h
@@ -2,14 +2,18 @@
 #define PROTOCOLCODES_H
 
 typedef enum: uint8_t {
-    UP = 1,
-    RIGHT,
-    LEFT,
-    DOWN,
-    SHOOT,
-    FIT_OUT,  // equipar, podemos ponerle un nombre mas acorde
-    KEY_UP,
-    KEY_DOWN,
-} KEYS;
+    Jump = 1,
+    MoveRight,
+    MoveLeft,
+    LookUp,
+    StopLookingUp,
+    Shoot,
+    StopShooting,
+    Crouch,  // agacharse
+    EquipWeapon,
+    UnequipWeapon,
+    StopMoving,
+
+} Commands;
 
 #endif
