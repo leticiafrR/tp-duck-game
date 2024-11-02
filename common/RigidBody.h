346ca4071763fa322bdfc508f19c62f5b4253477
Josue <nmartel@fi.uba.ar>
2024-11-02 09:12:27 -0300

    feat(physics): initial implementation for physics by RigidBody

---
+++ common/RigidBody.h
@@ -0,0 +1,35 @@
+#ifndef RIGID_BODY_H
+#define RIGID_BODY_H
+
+#include "Transform.h"
+
+#define GRAVITY 9.81f
+
+class RigidBody {
+private:
+    Transform& sourceT;
+    Vector2D velocity;
+    float mass;
+
+public:
+    RigidBody(Transform& transform, float mass): sourceT(transform), mass(mass) {}
+    ~RigidBody() = default;
+
+    // Reset gravity should be called once it's sure that the object is grounded
+    void ResetGravity() { velocity.y = 0; }
+
+    void ApplyForce(Vector2D force) { velocity += force; }
+
+    void Update(float deltaTime) {
+        Vector2D lastPos = sourceT.GetPos();
+
+        ApplyForce(Vector2D::Down() * GRAVITY * mass * deltaTime);  // Gravity
+
+        if (velocity.GetMagnitude() > 0)
+            velocity += (velocity.Normalized() * -0.1f * deltaTime);  // Friction
+
+        sourceT.Move(velocity * deltaTime);  // Movement update
+    }
+};
+
+#endif
