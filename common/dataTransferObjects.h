f1ae401316305155dec9c8d844f1451bd3e91170
AndreaFigueroaR <afigueroa@fi.uba.ar>
2024-11-05 13:56:20 -0300

    fefact(dtos): redefinition of data transfer objects

--- common/dataTransferObjects.h
+++ common/dataTransferObjects.h
@@ -1,72 +1,100 @@
 #ifndef DTO_H
 #define DTO_H
 
+#include <algorithm>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-#include "../common/Transform.h"
-#include "../common/Vector2D.h"
 #include "../data/id.h"
 
+#include "Transform.h"
+#include "Vector2D.h"
 
-struct MatchStartSettingsDto {
-    std::vector<PlayerID_t> playerIDs;
-    std::vector<std::string> playerNicknames;
-    std::vector<int> playerSkins;
+/******************************MATCH STARTING DATA**************************************/
+struct PlayerData {
+    PlayerID_t playerID;
+    uint8_t playerSkin;
+    std::string nickname;
+
+    // cppcheck-suppress passedByValue
+    PlayerData(PlayerID_t playerID, uint8_t playerSkin, std::string nickname):
+            playerID(playerID), playerSkin(playerSkin), nickname(nickname) {}
+};
+
+struct MatchStartDto {
+    std::vector<PlayerData> playersData;
     Vector2D duckSize;
 
-    MatchStartSettingsDto(int numberPlayers, Vector2D duckSize):
-            playerIDs(numberPlayers),
-            playerNicknames(numberPlayers),
-            playerSkins(numberPlayers),
-            duckSize(duckSize) {}
+    // cppcheck-suppress passedByValue
+    MatchStartDto(std::vector<PlayerData> playersData, Vector2D duckSize):
+            playersData(playersData), duckSize(duckSize) {}
+};
+
+/******************************GAME STARTING DATA**************************************/
+
+struct GroundDto {
+    typedef enum: uint8_t { RIGHT, LEFT, TOP, BOTTOM } VISIBLE_EDGES;
+    std::vector<VISIBLE_EDGES> visibleEdges;
+    Transform transform;
+
+    // cppcheck-suppress passedByValue
+    GroundDto(Transform transform, std::vector<VISIBLE_EDGES> visibleEdges):
+            transform(transform), visibleEdges(visibleEdges) {}
 };
 
 // no comunicar el valor de los angulos
-struct GameStartSettingsDto {
+struct GameSceneDto {
     std::string theme;
     std::vector<Transform> platforms;
-    // Maybe sould cointain a difference btwn the walls and the floor
+    std::vector<GroundDto> groundBlocks;
 
-    // cppcheck-suppress passedByValue
-    GameStartSettingsDto(std::string theme, std::vector<Transform> platforms):
-            theme(theme), platforms(platforms) {}
+    //  cppcheck-suppress passedByValue
+    GameSceneDto(std::string theme, std::vector<Transform> platforms,
+                 // cppcheck-suppress passedByValue
+                 std::vector<GroundDto> groundBlocks):
+            theme(theme), platforms(platforms), groundBlocks(groundBlocks) {}
+};
+
+/******************************GAME UPDATING DATA**************************************/
+
+struct DuckUpdate {
+    typedef enum: uint8_t { RIGHT, LEFT, UP } FLIP;
+    typedef enum: uint8_t { IDLE = 1, JUMP, FALLING, RUNNING, DEAD } DUCK_STATUS;
+    Vector2D position;
+    DUCK_STATUS status;
+    FLIP flip;
 };
 
 struct Snapshot {
     bool gameEnded;
-    std::unordered_map<PlayerID_t, Vector2D> positionsUpdate;
+    // skiping the part  of making a player quit out of the game
+    // std::vector<PlayerID_t> quiters;
+    std::unordered_map<PlayerID_t, DuckUpdate> ducksUpdate;
 
     // cppcheck-suppress passedByValue
-    Snapshot(bool gameEnded, std::unordered_map<PlayerID_t, Vector2D> positionsUpdate):
-            gameEnded(gameEnded), positionsUpdate(positionsUpdate) {}
+    Snapshot(bool gameEnded, std::unordered_map<PlayerID_t, DuckUpdate> ducksUpdate):
+            gameEnded(gameEnded), ducksUpdate(ducksUpdate) {}
 };
 
-typedef enum: uint8_t { MOVEMENT = 1, CONTROL_MATCH_STATE } MESSAGE_TYPE;
-
-// Note: the jump cmmd its not prolonged
-typedef enum: uint8_t {
-    LEFT_KEY_DOWN = 1,
-    LEFT_KEY_UP,
-    RIGHT_KEY_DOWN,
-    RIGHT_KEY_UP,
-    JUMP
-} MOVEMENT_IDS;
-
-typedef enum: uint8_t {
-    QUIT_MATCH = 1
-    //, CREATE_MATCH,JOIN_MMATCH, REFRESH
-} CONTROL_MATCH_STATE;
-
 struct Command {
-    uint8_t commandType;
+    typedef enum: uint8_t {
+        LEFT_KEY_DOWN = 1,
+        LEFT_KEY_UP,
+        RIGHT_KEY_DOWN,
+        RIGHT_KEY_UP,
+        JUMP,
+        QUIT
+    } COMMAND_ID;
+
     uint8_t commandID;
     uint32_t playerID;
 
-    Command(uint8_t commandType, uint8_t commandID, u_int32_t playerID = 0):
-            commandType(commandType), commandID(commandID), playerID(playerID) {}
+    explicit Command(uint8_t commandID, u_int32_t playerID = 0):
+            commandID(commandID), playerID(playerID) {}
 };
+
+/******************************GROUP GAMES RECOUNT DATA**************************************/
 
 struct GamesRecountDto {
     bool matchEnded;
