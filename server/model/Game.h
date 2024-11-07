788dab6d3929a0ff2e6fa05ad0a9c2617605bef4
leticia <leticia.figueroa.r07@gmail.com>
2024-11-07 10:36:29 -0300

    refactor(Game): updating some items uses for testing

--- server/model/Game.h
+++ server/model/Game.h
@@ -1,6 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <cmath>
 #include <iostream>
 #include <set>
 #include <string>
@@ -13,6 +14,7 @@
 
 #include "Duck.h"
 
+#define ID_PLAYER_UNIQUE_TEST 1
 class GameWorld {
 private:
     size_t livePlayers;
@@ -21,26 +23,69 @@ private:
     std::set<ObjectID_t> uniquesIds;
     EventsManager eventsManager;
     void ReapDead();
+    void Testing(const Vector2D& posToTest);
+    void CreatePlayers(const std::vector<PlayerID_t>& playersIds);
 
 public:
-    explicit GameWorld(const Vector2D& posToTest = Vector2D::Zero(),
-                       const std::vector<PlayerID_t>& playersIds = {1},
+    explicit GameWorld(const Vector2D& posToTest = Vector2D(INFINITY, INFINITY),
+                       const std::vector<PlayerID_t>& playersIds = {ID_PLAYER_UNIQUE_TEST},
                        const std::string& sceneName = "");
+
     void HandleCommand(const Command& cmd);
     void Update(float deltaTime);
     bool HasWinner();
     bool IsOver();
     PlayerID_t WhoWon();
     Snapshot GetSnapshot();
+
     // void quitPlayer(PlayerID_t);
     ~GameWorld();
 };
 
 
-/**********************************************************************************
+/***********************************************************************************
  *                              DEFINITIONS
  ***********************************************************************************/
 
+
+/* Players generators **************************************************************/
+
+void GameWorld::Testing(const Vector2D& posToTest) {
+    std::cout << "[TESTING]: Receiving unique player [" << ID_PLAYER_UNIQUE_TEST
+              << "]. Spawned in:(" << posToTest.x << "," << posToTest.y << ")\n";
+    players[ID_PLAYER_UNIQUE_TEST] = new Duck(posToTest);
+}
+
+void GameWorld::CreatePlayers(const std::vector<PlayerID_t>& playersIds) {
+    std::vector<Vector2D> spawnPoints = map.GetPlayersSpawnPoints();
+    for (size_t i = 0; i < spawnPoints.size(); i++) {
+        PlayerID_t id = playersIds[i];
+        Vector2D pos = spawnPoints[i];
+        std::cout << "Receiving player [" << id << "]. Spawned in:(" << pos.x << "," << pos.y
+                  << ")\n";
+        players[id] = new Duck(pos);
+    }
+}
+
+/**
+ * for testing, call like: GameWorld game = GameWorld(Vector2D(16, -13));
+ * the game'll create a *unique duck* with id 1 in the position indicated (maybe corrected by
+ * applying gravity (once at the start)).
+ */
+GameWorld::GameWorld(const Vector2D& posToTest, const std::vector<PlayerID_t>& playersIds,
+                     const std::string& sceneName):
+        livePlayers(playersIds.size()), uniquesIds(playersIds.begin(), playersIds.end()) {
+    std::cout << "Welcome to: " << sceneName << "\n";
+    if (std::isinf(posToTest.x)) {
+        CreatePlayers(playersIds);
+    } else {
+        Testing(posToTest);
+    }
+    eventsManager.SendListeners(players);
+}
+
+
+/* Game's logic **************************************************************/
 
 Snapshot GameWorld::GetSnapshot() { return eventsManager.GetSnapshot(IsOver()); }
 
@@ -60,22 +105,6 @@ void GameWorld::Update(float deltaTime) {
     ReapDead();
 }
 
-GameWorld::GameWorld(const Vector2D& posToTest, const std::vector<PlayerID_t>& playersIds,
-                     const std::string& sceneName):
-        livePlayers(playersIds.size()), uniquesIds(playersIds.begin(), playersIds.end()) {
-
-    if (posToTest.IsFarFrom(Vector2D::Zero())) {
-        players[1] = new Duck(posToTest);
-    } else {
-        std::vector<Vector2D> spawnPoints = map.GetPlayersSpawnPoints();
-        for (size_t i = 0; i < spawnPoints.size(); i++) {
-            std::cout << "Welcome to: " << sceneName << "\n";
-            players[playersIds[i]] = new Duck(spawnPoints[i]);
-        }
-    }
-
-    eventsManager.SendListeners(players);
-}
 
 GameWorld::~GameWorld() {
     for (auto& pair: players) {
@@ -87,7 +116,7 @@ GameWorld::~GameWorld() {
 void GameWorld::ReapDead() {
     for (auto it = players.begin(); it != players.end();) {
         if (it->second->IsDead()) {
-            // std::cout << "muriò el jugador con ID: [" << it->first << "]\n";
+            std::cout << "muriò el jugador con ID: [" << it->first << "]\n";
             delete it->second;
             it = players.erase(it);
             livePlayers--;
