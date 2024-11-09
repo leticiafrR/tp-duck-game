f7a76c021d6a169fde6f95adf6a2fce7281d7a5b
Josue <nmartel@fi.uba.ar>
2024-11-09 15:00:09 -0300

    fix(color): additive color fixed

--- client/ColorExtension.h
+++ client/ColorExtension.h
@@ -16,7 +16,7 @@ public:
     static Color Yellow() { return Color(255, 0, 255); }
 
     static Color AddValue(Color color, int value) {
-        return Color(std::clamp(color.a + value, 0, 255), std::clamp(color.g + value, 0, 255),
+        return Color(std::clamp(color.r + value, 0, 255), std::clamp(color.g + value, 0, 255),
                      std::clamp(color.b + value, 0, 255));
     }
 
