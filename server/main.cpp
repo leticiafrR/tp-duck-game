61726bb9a2bb1a6cd854f273917aa3ac470513d4
AndreaFigueroaR <afigueroa@fi.uba.ar>
2024-11-18 10:29:26 -0300

    refact(server/main): always use the 8080 port, not required as command line argument

--- server/main.cpp
+++ server/main.cpp
@@ -11,17 +11,12 @@
 
 void startDummyMatch(const char* port, Config& config);
 
-int main(int argc, const char* argv[]) {
+int main() {
     try {
-        if (argc >= 2) {
-            Config config;
-            startDummyMatch(argv[1], std::ref(config));
-            return 0;
-
-        } else {
-            std::cerr << "Bad program call. Expected " << argv[0] << " <servername>" << std::endl;
-            return 1;
-        }
+        Config config;
+        auto port = "8080";
+        startDummyMatch(port, std::ref(config));
+        return 0;
     } catch (const std::exception& err) {
         std::cerr << "Something went wrong and an exception was caught: " << err.what()
                   << std::endl;
