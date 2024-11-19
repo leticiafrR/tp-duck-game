6fc691fed4bf8e1f5f6866904bbe796247ba242a
candematelica <candela.matelica@gmail.com>
2024-11-19 15:15:36 -0300

    fix(scripts, external): Include libSDL2pp library. Change scripts/install.sh to recognize /scripts/files directory. Include the instruction add_subdirectory( external/libSDL2pp) at CMakeLists

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,11 +28,17 @@ sudo apt-get update
           libjpeg-dev \
           libpng-dev \
           libfreetype-dev
+          
+print_colored_message() {
+    color=$1
+    shift
+    printf "\e[${color}m$@\e[0m\n"
+}
 
-# Install the dependencies for C++
-#sudo apt-get install -y build-essential g++ cmake libjpeg-dev libpng-dev \
-#libfreetype-dev libopusfile-dev libflac-dev libxmp-dev libfluidsynth-dev \
-#libwavpack-dev libmodplug-dev libsdl2-dev libyaml-dev
+# List of available colors
+RED="31";GREEN="32";GREEN_BOLD="1;32";YELLOW="33";YELLOW_BOLD="1;33";
+
+print_colored_message $YELLOW_BOLD "══ Adding CMake files for SDL2 packages in /usr/lib/x86_64-linux-gnu/cmake/ ══"
 
 SCRIPT_DIR="$(dirname "$0")"
 #sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
@@ -71,39 +77,6 @@ if [ ! -e "/usr/lib/x86_64-linux-gnu/cmake/SDL2_ttf/sdl2_ttf-config.cmake" ]; th
     print_colored_message $YELLOW_BOLD "══ Copied sdl2_ttf-config.cmake ══"
 fi
 
-# Move to the external directory where all dependencies are located
-cd external
-
-# Install SDL_mixer
-cd SDL_mixer
-mkdir -p build
-cd build
-cmake ..
-make
-sudo make install
-cd ../..
-
-# Install SDL_ttf
-cd SDL_ttf
-mkdir -p build
-cd build
-cmake ..
-make
-sudo make install
-cd ../..
-
-# Install libsdl2pp
-cd SDL2pp
-mkdir -p build
-cd build
-cmake ..
-make
-sudo make install
-cd ../..
-
-# Go back to the root directory
-cd ..
-
 # Check if the required packages were installed
 echo "Checking installed packages..."
 dpkg -s libsdl2-dev libyaml-dev
@@ -114,6 +87,6 @@ if [ -f "Makefile" ]; then
 else
     echo "No Makefile found. Skipping clean step."
 fi
-
+cd ..
 # Build the project
 make
