653ad293b9c0bf1a3e35f3641184580dca9dd216
candematelica <candela.matelica@gmail.com>
2024-11-22 10:39:54 -0300

    fix(server): Add file to manage constants and include duck and weapon config in YAML file

---
+++ server/constantServer.h
@@ -0,0 +1,24 @@
+#ifndef CONSTANTSERVER_H
+#define CONSTANTSERVER_H
+
+#include <string>
+
+/************************************STATIC MAP*************************************/
+const std::string THEME_STR = "theme";
+const std::string FULL_MAP_STR = "full_map_size";
+const std::string X_STR = "x";
+const std::string Y_STR = "y";
+const std::string PLAYERS_POINTS_STR = "players_spawn_points";
+const std::string PLATAFORM_STR = "plataforms";
+const std::string EDGES_STR = "edges";
+const std::string WEIGHT_STR = "w";
+const std::string HIGH_STR = "h";
+const std::string LEFT_STR = "LEFT";
+const std::string RIGHT_STR = "RIGHT";
+const std::string BOTTOM_STR = "BOTTOM";
+const std::string TOP_STR = "TOP";
+
+const float HALF = 2;
+const float FALLING = -1;
+
+#endif
--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,9 @@
+#include "constant.h"
 #include "server.h"
 
 int main() {
     Server server;
     server.keepServing();
     server.closeServing();
-    return 0;
+    return SUCCESS_EXIT;
 }
