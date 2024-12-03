351f1a8ed811d8bfe2a24fd526e566d43ac3389e
leticia <leticia.figueroa.r07@gmail.com>
2024-12-03 08:32:12 -0300

    refactor(Equipment): separating deployment into .cpp file

---
+++ server/model/Equipment.cpp
@@ -0,0 +1,29 @@
+#include "Equipment.h"
+Equipment::Equipment(): armor(TypeItem::EMPTY), helmet(TypeItem::EMPTY) {}
+bool Equipment::HasArmor() { return (armor != TypeItem::EMPTY); }
+bool Equipment::HasHelmet() { return (helmet != TypeItem::EMPTY); }
+bool Equipment::TryEquip(TypeItem& item) {
+
+    if (item == TypeItem::ARMOR && !HasArmor()) {
+        armor = item;
+        item = TypeItem::EMPTY;
+        return true;
+    }
+    if (item == TypeItem::HELMET && !HasHelmet()) {
+        helmet = item;
+        item = TypeItem::EMPTY;
+        return true;
+    }
+    return false;
+}
+
+bool Equipment::AbsorbDamage() {
+    if (HasHelmet()) {
+        helmet = TypeItem::EMPTY;
+    } else if (HasArmor()) {
+        armor = TypeItem::EMPTY;
+    } else {
+        return false;
+    }
+    return true;
+}
--- server/model/Equipment.h
+++ server/model/Equipment.h
@@ -8,32 +8,10 @@ private:
     TypeItem helmet;
 
 public:
-    Equipment(): armor(TypeItem::EMPTY), helmet(TypeItem::EMPTY) {}
-    bool HasArmor() { return (armor != TypeItem::EMPTY); }
-    bool HasHelmet() { return (helmet != TypeItem::EMPTY); }
-    bool TryEquip(TypeItem& item) {
-
-        if (item == TypeItem::ARMOR && !HasArmor()) {
-            armor = item;
-            item = TypeItem::EMPTY;
-            return true;
-        }
-        if (item == TypeItem::HELMET && !HasHelmet()) {
-            helmet = item;
-            item = TypeItem::EMPTY;
-            return true;
-        }
-        return false;
-    }
-    bool AbsorbDamage() {
-        if (HasHelmet()) {
-            helmet = TypeItem::EMPTY;
-        } else if (HasArmor()) {
-            armor = TypeItem::EMPTY;
-        } else {
-            return false;
-        }
-        return true;
-    }
+    Equipment();
+    bool HasArmor();
+    bool HasHelmet();
+    bool TryEquip(TypeItem& item);
+    bool AbsorbDamage();
 };
 #endif
